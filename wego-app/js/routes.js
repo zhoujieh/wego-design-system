f72be559cef1779386e8a946adef95c4a4bbbe57
Jee.zhou <ringcrop@gmail.com>
2026-07-12 15:46:34 +0800

    优化一系列内容

--- wego-app/js/routes.js
+++ wego-app/js/routes.js
@@ -1,1 +1,18 @@
-window.WEGO_APP_ROUTES = [];
+window.WEGO_APP_ROUTES = [
+  {
+    routeId: 'dongtai-feed',
+    scene: '动态商品流',
+    script: './scenes/动态商品流/scene.js',
+    style: './scenes/动态商品流/scene.css',
+    entry: {
+      type: 'host-tab',
+      tab: 'dongtai'
+    }
+  },
+  {
+    routeId: 'dongtai-product-detail',
+    scene: '动态商品流',
+    script: './scenes/动态商品流/scene.js',
+    style: './scenes/动态商品流/scene.css'
+  }
+];
