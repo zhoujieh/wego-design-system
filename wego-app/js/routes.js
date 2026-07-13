af0fe2cce0686e0e2129fbaa02d10417c2cf5fce
zhoujieh <ringcrop@gmail.com>
2026-07-13 12:22:08 +0800

    新增我的页面场景
    参考真实产品截图1:1对齐布局，包含用户信息区、会员区、快捷入口、功能入口、我买的、设置列表、提现区七个分区，35个入口绑定toast反馈。

--- wego-app/js/routes.js
+++ wego-app/js/routes.js
@@ -1,1 +1,9 @@
-window.WEGO_APP_ROUTES = [];
+window.WEGO_APP_ROUTES = [
+  {
+    routeId: 'my',
+    scene: '我的',
+    entry: { type: 'host-tab', tab: 'my' },
+    script: 'scenes/我的/scene.js',
+    style: 'scenes/我的/scene.css'
+  }
+];
