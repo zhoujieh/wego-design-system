a8aa9a33bc254334839c5e2fd6dfbe5eb1ec9959
zhoujieh <ringcrop@gmail.com>
2026-07-13 21:59:25 +0800

    新增我的tab页面

--- wego-app/js/routes.js
+++ wego-app/js/routes.js
@@ -1,1 +1,12 @@
-window.WEGO_APP_ROUTES = [];
+window.WEGO_APP_ROUTES = [
+  {
+    routeId: 'my-tab',
+    scene: '我的tab',
+    entry: {
+      type: 'host-tab',
+      tab: 'my'
+    },
+    script: './scenes/我的tab/scene.js',
+    style: './scenes/我的tab/scene.css'
+  }
+];
