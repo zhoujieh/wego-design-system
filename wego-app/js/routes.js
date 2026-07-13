3b9eeb889750a8b0973d4cb7d9bc02de276e2c36
zhoujieh <ringcrop@gmail.com>
2026-07-13 18:00:49 +0800

    优化工作流

--- wego-app/js/routes.js
+++ wego-app/js/routes.js
@@ -1,8 +1,1 @@
-window.WEGO_APP_ROUTES = [
-  {
-    routeId: 'my',
-    entry: { type: 'host-tab', tab: 'my' },
-    script: './scenes/我的/scene.js',
-    style: './scenes/我的/scene.css'
-  }
-];
+window.WEGO_APP_ROUTES = [];
