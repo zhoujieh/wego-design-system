d7832eadbbc4e0914f7fb8334d2aac212014e3d2
Jee.zhou <ringcrop@gmail.com>
2026-07-14 13:27:51 +0800

    完成「我的」页面原型开发

--- wego-app/js/routes.js
+++ wego-app/js/routes.js
@@ -12,5 +12,12 @@ window.WEGO_APP_ROUTES = [
     entry: { type: 'push', parentEntry: 'dongtai', label: '产品详情' },
     script: './scenes/动态商品流/scene.js',
     style: './scenes/动态商品流/scene.css'
+  },
+  {
+    routeId: 'wode',
+    scene: '我的',
+    entry: { type: 'host-tab', tab: 'my' },
+    script: './scenes/我的/scene.js',
+    style: './scenes/我的/scene.css'
   }
 ];
