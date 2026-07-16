51b90624855009b13950ffac3a19f1718eda2c60
Jee.zhou <ringcrop@gmail.com>
2026-07-16 10:02:08 +0800

    新增我的页基准原型 (#6)
    新增“我的”Tab、响应式应用入口、应用中心二级页及对应迭代与设计决策证据。

--- wego-app/js/routes.js
+++ wego-app/js/routes.js
@@ -1,1 +1,13 @@
-window.WEGO_APP_ROUTES = [];
+window.WEGO_APP_ROUTES = [
+  {
+    routeId: 'my',
+    scene: '我的',
+    script: 'scenes/我的/scene.js',
+    style: 'scenes/我的/scene.css',
+    entry: {
+      type: 'host-tab',
+      tab: 'my',
+      label: '我的'
+    }
+  }
+];
